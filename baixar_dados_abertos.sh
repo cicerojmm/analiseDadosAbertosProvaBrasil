834e7bb7f0d506a5f6f6c9f4b70b8b19d7b4a4e3
Cícero Joasyo Mateus de Moura <cicerojmm@gmail.com>
2020-10-12 11:55:01 -0300

    Update baixar_dados_abertos.sh

--- baixar_dados_abertos.sh
+++ baixar_dados_abertos.sh
@@ -1,2 +1,8 @@
-wget ftp://ftp.inep.gov.br/microdados/microdados_prova_brasil_2011.zip && unzip microdados_prova_brasil_2011.zip && mkdir dags/dados && mv Microdados\ Prova\ Brasil\ 2011/Dados/TS_RESULTADO_ALUNO.csv dags/dados/ && mv Microdados\ Prova\ Brasil\ 2011/Dados/TS_RESULTADO_MUNICIPIO.csv dags/dados/ && 
-rm -rf Microdados\ Prova\ Brasil\ 2011/ && rm -rf microdados_prova_brasil_2011.zip
+#!/bin/bash
+wget ftp://ftp.inep.gov.br/microdados/microdados_prova_brasil_2011.zip
+unzip microdados_prova_brasil_2011.zip
+mkdir dags/dados
+mv Microdados\ Prova\ Brasil\ 2011/Dados/TS_RESULTADO_ALUNO.csv dags/dados/
+mv Microdados\ Prova\ Brasil\ 2011/Dados/TS_RESULTADO_MUNICIPIO.csv dags/dados/
+rm -rf Microdados\ Prova\ Brasil\ 2011/
+rm -rf microdados_prova_brasil_2011.zip
